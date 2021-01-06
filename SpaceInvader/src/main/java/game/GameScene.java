56cd594dd65dd4d440558116ac1c646a4d79d966
Uchihalucky <luckybairagi96@gmail.com>
2021-01-06 22:00:36 +0530

    GameStart Menu / No Controls after or before Game start and end / Separate classes for all

--- SpaceInvader/src/main/java/game/GameScene.java
+++ SpaceInvader/src/main/java/game/GameScene.java
@@ -4,25 +4,21 @@ package game;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+
 
 public class GameScene extends Application {
 
@@ -45,101 +41,144 @@ public class GameScene extends Application {
     private boolean isPause = false;
     private int SCORE;
     private Text score = new Text();
+    private Hero hero;
+    private AnimationTimer timer;
+    private GameScene gameScene = this;
+    private boolean collide = true;
+    private boolean START = false;
 
 
+    public void gameReset(){
+        Iterator<Space> elementsItr = elements.iterator();
+        while (elementsItr.hasNext()) {
+            Space space = elementsItr.next();
+            pane.getChildren().remove(space);
+            elementsItr.remove();
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+        }
+        Iterator<HeroBullets> bulletsItr = bulletsList.iterator();
+        while (bulletsItr.hasNext()){
+            HeroBullets heroBullets = bulletsItr.next();
+            pane.getChildren().remove(heroBullets);
+            bulletsItr.remove();
+        }
+        Iterator<Enemies> iterator = enemiesList.iterator();
+        while (iterator.hasNext()) {
+          Enemies enemies = iterator.next();
+          pane.getChildren().remove(enemies);
+          iterator.remove();
 
-        Hero hero = new Hero();
-        pane.getChildren().addAll(hero, score);
-        score.setFont(new Font("Monospaced", 25));
-        score.setFill(Color.rgb(255,255,255, 0.7));
-        score.setX(1);
-        score.setY(25);
-        score.setText("SCORE : "+ SCORE);
+        }
+
+        elements.clear();
+        bulletsList.clear();
+        enemiesList.clear();
+        collide = true;
+        SCORE = 0;
+        ENEMIES_SPEED = 3;
+        SPACE_SPEED = 2.0;
+        pane.getChildren().remove(score);
+        score.setText("SCORE : " + SCORE);
+        pane.getChildren().add(score);
+
+    }
+
+    public void gameScene() throws Exception {
 
 
-        AnimationTimer timer = new AnimationTimer() {
+            hero = new Hero();
+            pane.getChildren().addAll(hero, score);
+            score.setFont(new Font("Monospaced", 25));
+            score.setFill(Color.rgb(255, 255, 255, 0.7));
+            score.setX(1);
+            score.setY(25);
+            score.setText("SCORE : " + SCORE);
+
+
+            timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                System.out.println(SPACE_SPEED);
-                if (elements.size() < 50 || now%2== 0){
+
+                if (elements.size() < 50 || now % 2 == 0) {
                     space = new Space();
                     pane.getChildren().add(space);
                     elements.add(space);
                 }
-                for (Space elements: elements) {
+                for (Space elements : elements) {
                     elements.move(SPACE_SPEED);
                 }
-                Iterator<Space> elementsItr = elements.iterator();
-                while (elementsItr.hasNext()){
-                    Space space = elementsItr.next();
-                    if (space.getCenterY() > SCENE_HEIGHT){
-                        pane.getChildren().remove(space);
-                        elementsItr.remove();
-                    }
-                }
-
-                for (Enemies enemies: enemiesList) {
-                    enemies.move(ENEMIES_SPEED);
-                    if (enemies.collide(hero)) {
-                        pane.getChildren().remove(hero);
-                        GAME_OVER = true;
-                    }
-                }
-
-                Iterator<Enemies> itr = enemiesList.iterator();
-                while (itr.hasNext()){
-                    Enemies enemies = itr.next();
-
-                    Iterator<HeroBullets> iterator = bulletsList.iterator();
-                    while (iterator.hasNext()){
-                        HeroBullets bullets = iterator.next();
-                        if (bullets.collide(enemies)){
-                            SCORE +=1;
-                            score.setText("SCORE : "+ SCORE);
-                            iterator.remove();
-                            itr.remove();
-                            pane.getChildren().remove(bullets);
-                            pane.getChildren().remove(enemies);
+                if (START){
+                    Iterator<Space> elementsItr = elements.iterator();
+                    while (elementsItr.hasNext()) {
+                        Space space = elementsItr.next();
+                        if (space.getCenterY() > SCENE_HEIGHT) {
+                            pane.getChildren().remove(space);
+                            elementsItr.remove();
                         }
                     }
-                    if (enemies.getPosY() > SCENE_HEIGHT){
 
-                        pane.getChildren().remove(enemies);
-                        itr.remove();
+                    for (Enemies enemies : enemiesList) {
+                        enemies.move(ENEMIES_SPEED);
+                        if (enemies.collide(hero) && collide) {
+                            pane.getChildren().remove(hero);
+                            GAME_OVER = true;
+                        }
                     }
 
-                }
-                if (enemiesList.size() < 5 || now%71 == 0){
-                    try {
-                        enemies = new Enemies(posX[random.nextInt(7)], -10, 50, 90);
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
+                    Iterator<Enemies> itr = enemiesList.iterator();
+                    while (itr.hasNext()) {
+                        Enemies enemies = itr.next();
+
+                        Iterator<HeroBullets> iterator = bulletsList.iterator();
+                        while (iterator.hasNext()) {
+                            HeroBullets bullets = iterator.next();
+                            if (bullets.collide(enemies)) {
+                                SCORE += 1;
+                                score.setText("SCORE : " + SCORE);
+                                iterator.remove();
+                                itr.remove();
+                                pane.getChildren().remove(bullets);
+                                pane.getChildren().remove(enemies);
+                            }
+                        }
+                        if (enemies.getPosY() > SCENE_HEIGHT) {
+
+                            pane.getChildren().remove(enemies);
+                            itr.remove();
+                        }
+
                     }
-                    enemiesList.add(enemies);
-                    pane.getChildren().add(enemies.show());
+                    if (enemiesList.size() < 5 || now % 71 == 0) {
+                        try {
+                            enemies = new Enemies(posX[random.nextInt(7)], -10, 50, 90);
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
+                        enemiesList.add(enemies);
+                        pane.getChildren().add(enemies.show());
+                    }
+
+                    for (HeroBullets bullets : bulletsList) {
+                        bullets.move();
+                        if (SCORE > 50) {
+                            bullets.upgrade();
+                        }
+                    }
+
+                    if (GAME_OVER) {
+                        GameOverScreen gameOverScreen = new GameOverScreen(SCORE, gameScene, stackPane);
+                        stackPane.getChildren().add(gameOverScreen);
+                        for (HeroBullets bullets : bulletsList) {
+                            pane.getChildren().remove(bullets);
+                        }
+                        bulletsList.clear();
+                        GAME_OVER = false;
+                        collide = false;
+                    }
+                    SPACE_SPEED += 0.002;
+                    ENEMIES_SPEED += 0.002;
                 }
 
-                for (HeroBullets bullets : bulletsList){
-                    bullets.move();
-                    if (SCORE > 50){
-                        bullets.upgrade();
-                    }
-                }
-
-                if (GAME_OVER){
-                    GameOverScreen gameOverScreen = new GameOverScreen();
-                    stackPane.getChildren().add(gameOverScreen);
-                    gameOverScreen.setVisible(true);
-                    for (HeroBullets bullets : bulletsList){
-                        pane.getChildren().remove(bullets);
-                    }
-                    bulletsList.clear();
-                }
-                SPACE_SPEED += 0.002;
-                ENEMIES_SPEED += 0.002;
             }
 
 
@@ -147,40 +186,78 @@ public class GameScene extends Application {
         timer.start();
 
 
+    }
+
+    public void gameStartMenu(){
+        VBox vBox = new VBox();
+
+        vBox.setTranslateX((SCENE_WIDTH/2)-50);
+        vBox.setTranslateY((SCENE_HEIGHT/2)-50);
+
+        Text newGame = new Text("New Game");
+        newGame.setFill(Color.WHITE);
+        newGame.setFont(new Font(20));
+        newGame.setX(vBox.getMaxWidth()/2);
+        newGame.setY(vBox.getMaxHeight()/2);
+        newGame.setOnMouseEntered(e->{
+           newGame.setFill(Color.RED);
+        });
+        newGame.setOnMouseExited(e->{
+            newGame.setFill(Color.WHITE);
+        });
+        newGame.setOnMouseClicked(e ->{
+            gameReset();
+            START = true;
+            stackPane.getChildren().remove(vBox);
+            pane.getChildren().add(hero);
+
+        });
+        vBox.getChildren().add(newGame);
+        stackPane.getChildren().add(vBox);
+    }
 
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-        pauseScreen.setVisible(false);
-        stackPane.getChildren().addAll(pane, pauseScreen);
+        gameScene();
+
+        stackPane.getChildren().addAll(pane);
 
         Scene scene =new Scene(stackPane, SCENE_WIDTH, SCENE_HEIGHT);
         scene.setOnKeyPressed(e ->{
-	    if(!GAME_OVER){
+	    if(!GAME_OVER && collide && START){
            	 if (e.getCode() == KeyCode.P)
-              	   checkPause(timer);
+              	   checkPause();
            	 else
                	 controls(e.getCode(), hero);
 	    }
 
         });
 
+
         scene.setFill(Color.BLACK);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        gameStartMenu();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-    public void checkPause(AnimationTimer timer){
+    public void checkPause() {
         if (isPause == false) {
+            stackPane.getChildren().add(pauseScreen);
             timer.stop();
             isPause = true;
             pauseScreen.setVisible(true);
         }else if (isPause == true){
-            timer.start();
             isPause = false;
             pauseScreen.setVisible(false);
+            stackPane.getChildren().remove(pauseScreen);
+            timer.start();
+
         }
     }
 
@@ -189,137 +266,22 @@ public class GameScene extends Application {
             hero.setTranslateX(hero.getHeroX() + 10);
         if (keyCode == KeyCode.LEFT)
             hero.setTranslateX(hero.getHeroX() - 10);
-        if (keyCode == KeyCode.SPACE){
+
+        if (keyCode == KeyCode.SPACE) {
             bullets = new HeroBullets(hero);
             bulletsList.add(bullets);
             pane.getChildren().add(bullets);
-        }
-
-    }
-
-    public class Enemies extends ImageView {
-        private Image[] images = new Image[]{
-                new Image(String.valueOf(new File("src/main/resources/enemtship3.png").toURI().toURL())),
-                new Image(String.valueOf(new File("src/main/resources/enemyShip2.png").toURI().toURL())),
-                new Image(String.valueOf(new File("src/main/resources/enemyShip4.png").toURI().toURL())),
-                new Image(String.valueOf(new File("src/main/resources/enemyShip5.png").toURI().toURL())),
-                new Image(String.valueOf(new File("src/main/resources/enemyShip1.png").toURI().toURL()))
-        };
-
-        Enemies(double posX, double posY, double height, double width) throws MalformedURLException {
-            this.setImage(images[random.nextInt(5)]);
-            this.setFitWidth(width);
-            this.setFitHeight(height);
-            this.setTranslateX(posX);
-            this.setTranslateY(posY);
-        }
-
-        public void move(double SPEED){
-            this.setTranslateY(this.getTranslateY()+SPEED);
-        }
-
-        public Enemies show(){
-            return this;
-        }
-        public double getPosY(){
-            return this.getTranslateY();
-        }
-        public boolean collide(Hero hero){
-            boolean isCollide = false;
-            if (hero.getBoundsInParent().intersects(this.getBoundsInParent())) {
-                isCollide = true;
             }
-            return isCollide;
-        }
-
-    }
-
-    public class Space extends Circle{
-        int RADIUS = random.nextInt(3);
-        Space(){
-            this.setRadius(RADIUS);
-            this.setFill(Color.rgb(255, 255, 255, 0.5));
-            this.setCenterX(random.nextInt(SCENE_WIDTH));
-            this.setCenterY(0);
-        }
-
-        public void move(double SPEED){
-            this.setCenterY(this.getCenterY() + SPEED);
-        }
-    }
-
-    public class Hero extends ImageView{
-        Hero() throws Exception{
-            this.setImage(new Image(String.valueOf(new File("src/main/resources/heroSpaceShip.png").toURI().toURL())));
-            this.setFitHeight(50);
-            this.setFitWidth(100);
-            this.setTranslateX(540);
-            this.setTranslateY(650);
-        }
-        public double getHeroX(){
-            return this.getTranslateX();
         }
 
 
-    }
-    public class HeroBullets extends Rectangle{
-        HeroBullets(Hero hero){
-            this.setHeight(3);
-            this.setWidth(3);
-            this.setFill(Color.RED);
-            this.setTranslateX(hero.getTranslateX()+(hero.getFitWidth()/2));
-            this.setTranslateY(hero.getTranslateY()-10);
-        }
-        public void move(){
-            this.setTranslateY(this.getTranslateY()-5);
-        }
-        public boolean collide(Enemies enemies){
-            boolean isCollide = false;
-            if (enemies.getBoundsInParent().intersects(this.getBoundsInParent())){
-                isCollide = true;
-            }
-            return isCollide;
-        }
-        public void upgrade(){
-            this.setHeight(8);
-            this.setWidth(8);
-            this.setFill(Color.DARKBLUE);
-        }
-    }
-    public class PauseScreen extends StackPane{
-        private Rectangle rectangle;
-        PauseScreen(){
-            this.setHeight(SCENE_HEIGHT-100);
-            this.setWidth(SCENE_WIDTH -100);
-            rectangle = new Rectangle(this.getWidth(), this.getHeight(), Color.rgb(255,255,255,0.2));
-            Text head = new Text("PAUSED");
-            head.setFont(new Font(25));
-            this.getChildren().addAll(rectangle,head);
-        }
-    }
 
-    public class GameOverScreen extends VBox {
-        private Rectangle rectangle;
-        GameOverScreen(){
-            this.setHeight(SCENE_HEIGHT-200);
-            this.setWidth(SCENE_WIDTH - 200);
-            this.setTranslateX((SCENE_WIDTH/2)-100);
-            this.setTranslateY((SCENE_HEIGHT/2)-50);
 
-            rectangle = new Rectangle(this.getWidth(), this.getHeight(), Color.rgb(255,255,255,0.2));
-            Text head = new Text("GAME OVER");
-            head.setFont(new Font(25));
-            head.setFill(Color.WHITE);
-            head.setX(this.getWidth()/2);
-            head.setY((this.getHeight()/2));
 
-            Text scoreToShow = new Text("Your Score is : "+ SCORE);
-            scoreToShow.setFont(new Font(20));
-            scoreToShow.setFill(Color.WHITE);
-            scoreToShow.setX(this.getWidth()/2);
-            scoreToShow.setY((this.getHeight()/2)+50);
 
-            this.getChildren().addAll(head, scoreToShow);
-        }
-    }
+
+
+
+
+
 }
